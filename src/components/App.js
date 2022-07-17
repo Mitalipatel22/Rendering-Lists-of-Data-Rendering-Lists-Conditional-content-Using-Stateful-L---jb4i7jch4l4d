0d3936e4b67573d8872933b65fdcbeff41bdb9e8
Mitalipatel22 <vamjamitali2824@gmail.com>
2022-07-17 11:46:01 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      renderBall: false, //variable needed to be changed
-      posi: 0,
-      ballPosition: { left: "0px" }
-    };
-    this.renderChoice = this.renderChoice.bind(this);
-    this.buttonClickHandler = this.buttonClickHandler.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
-  //call back function
-  handleKeyDown(event) {
-    if (event.keyCode === 39) {
-      const value = this.state.posi + 5;
-      // console.log(5);
-      this.setState({
-        ballPosition: { left: value + "px" },
-        posi: value
-      });
-    }
-  }
-  buttonClickHandler() {
-    this.setState({ renderBall: true });
-  }
-  renderChoice() {
-    if (this.state.renderBall) {
-      return <div className="ball" style={this.state.ballPosition}></div>;
-    } else
-      return (
-        <button onClick={this.buttonClickHandler}>Click For One Ball</button>
-      );
-  }
-
-  //bind ArrowRight keydown event
-
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown);
-  }
-
   render() {
-    return <div className="playground">{this.renderChoice()}</div>;
+    let relatives = ["Disha", "krina", "priya", "Mitali"];
+    return (
+      <>
+        <div id="main">{/* Do not remove the main div */}</div>
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={"relativeListItem" + (index + 1)}>{relative}</li>
+          ))}
+        </ol>
+      </>
+    );
   }
 }
 
